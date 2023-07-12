7019dc60c52d5ccbaf56fc7148078d322d3309d9
mandorakannu <mandorakannu@gmail.com>
2023-07-13 00:04:35 +0530

    create dynamic route of posts/[id]

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,25 @@
+import posts from "@jsons/posts.json";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: { id: string } },
+  response: NextResponse
+) {
+  const { id } = context.params;
+  const _id = parseInt(id);
+  const post = posts.filter((post) => post.id === _id);
+  if (post.length === 0) {
+    return NextResponse.json(
+      {
+        error: `Only 100 posts are available. Read Docs: ${process.env.VERCEL_URL}/docs`,
+      },
+      {
+        status: 404,
+        statusText: "post Not Found",
+      }
+    );
+  } else {
+    return NextResponse.json({ post: post[0] }, { status: 200 });
+  }
+}
